8ccf5a42e503c9112e09ae02b8f5c9d6a417ceb3
Code2Change1 <ekkothegamer33@gmail.com>
2025-07-29 19:18:31 +0000

    added java script components

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("wsp guys im mr bruno")
+
+let chicken=123;
+console.log(chicken);
+console.log(typeof chicken);
+
+console.log(10+" eggs");
+console.log(10+8}+ "eggs");
+console.log("eggs"+10+8);
+console.log(8!=="82")
+console.log(7>3)&&)(8>5);
+console.log(10>3)||(5>10);
+console.log(!5>3)
+
+const hour = new Date().getHours();
+let greeting = "";
+if (hour < 18) {
+
+} else if (hour < 18) { 
+    greeting = "Good evening";
+}
+document.getElementById("greeting").innerText = greeting
